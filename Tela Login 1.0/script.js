9e9aeaa58af80879aae47366e8056c458c60a7f6
Vitor Geordane <vitorgeordane032@gmail.com>
2024-12-02 22:32:49 -0300

    Add files via upload

---
+++ Tela Login 1.0/script.js
@@ -0,0 +1,10 @@
+const logregBox = document.querySelector('logregBox');
+
+const loginLink = document.querySelector('loginLink');
+
+const registerLink = document.querySelector(' registerLink');
+
+
+registerLink.addEventListener('click', () => {
+    logregBox.classList.add('activate')
+})
